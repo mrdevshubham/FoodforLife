18f26273f2cb535a7d1b31457d171e95e7827139
Shubham Jain <shubham.netdev@gmail.com>
2018-04-05 12:30:17 +0530

    First Code Check In

---
+++ FoodForLife/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FoodForLife.MvcApplication" Language="C#" %>
---
+++ FoodForLife/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FoodForLife.Startup))]
+namespace FoodForLife
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //ConfigureAuth(app);
+        }
+    }
+}
