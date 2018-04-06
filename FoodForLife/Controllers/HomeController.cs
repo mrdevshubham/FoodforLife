e275bf4c3ec7548a5077620736f2a685da2ac418
HKT8\HKT8 <shubham.netdev@gmail.com>
2018-04-06 17:13:00 +0530

    Login Endpoint, Vendor List, Save Vendor, Save Donation Request, List Donation Request, Donation Details, Vendor Details.

--- FoodForLife/Controllers/HomeController.cs
+++ FoodForLife/Controllers/HomeController.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using FoodForLife.Data;
+using FoodForLife.Business;
+using FoodForLife.Model;
 
 namespace FoodForLife.Controllers
 {
@@ -11,30 +12,35 @@ namespace FoodForLife.Controllers
     {
         public ActionResult Index()
         {
-
-           
-
             return View();
         }
 
         [HttpPost]
-        public ActionResult Index(string email, string password)
+        public ActionResult SignIn(string email, string password)
         {
-            bool Res = (new LoginDAL()).Authenticate(email, password);
-            return Json( new { Result = "" } );
+            if (!string.IsNullOrEmpty(email) || !string.IsNullOrEmpty(password))
+                return Json(new { Result = "Failure", Message = "Either Email or Password field is blank, Please fill all the details." });
+            else
+            {
+                clsUser oclsUser = new clsUser();
+                bool IsValidUser = (new LoginBAL()).Authenticate(email, password, ref oclsUser);
+                Session["user"] = oclsUser;
+                if (IsValidUser)
+                    return Json(new { Result = "Success" });
+                else
+                    return Json(new { Result = "Failure", Message = "The Email/Password combination is not correct." });
+            }
         }
 
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
