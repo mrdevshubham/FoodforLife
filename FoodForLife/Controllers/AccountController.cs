3cd1328d34807c6055f00c98d324808438db6dfd
HKT8\HKT8 <shubham.netdev@gmail.com>
2018-04-08 01:06:18 +0530

    gh

--- FoodForLife/Controllers/AccountController.cs
+++ FoodForLife/Controllers/AccountController.cs
@@ -4,12 +4,47 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using FoodForLife.Business;
+using FoodForLife.Model;
+
 namespace FoodForLife.Controllers
 {
     public class AccountController : Controller
     {
         // GET: Account
-        
+
+        ResponseMessage oResponse = new ResponseMessage();
+
+        public ActionResult Home(string Id)
+        {
+            if (Id == RequestStatus.NEW || Id == RequestStatus.PENDING || Id == RequestStatus.COMPLETED)
+            {
+                ViewBag.RequestStatus = Id;
+                HomeModel oHomeModel = new HomeModel();
+                oHomeModel.lstDonationDetails = (new DonorBAL()).GetDonorBAL(Id);
+                oHomeModel.lstVendors = (new VendorBAL().GetVendorBAL(0));
+                return View(oHomeModel);
+            }
+            else
+            {
+                return RedirectToAction("Home",new { Id = RequestStatus.NEW });
+            }
+        }
+
+
+        public ActionResult AssignVendor(long RequestId, long VendorId)
+        {
+
+
+
+            oResponse.Result = "Success";
+            oResponse.Message = "Vendor assigned successfully.";
+            return Json(new { oResponse });
+
+        }
+
+
+
 
     }
 }
